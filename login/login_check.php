683285d88515a468ef3c07b698e2b434ecee5859
Matt Hunt <47961957+AlmostHDcode@users.noreply.github.com>
2024-08-03 17:39:49 -0400

    Update login_check.php

--- login/login_check.php
+++ login/login_check.php
@@ -3,19 +3,16 @@ $now = time();
 
 if(!isset($_SESSION['login']) || $_SESSION['login'] != 'success') {
     $logout_status = 'skip';
-    require_once(LOGOUT);
+    require_once LOGOUT;
 }
 
 if(isset($_SESSION['session_expire']) && $now <= $_SESSION['session_expire']) {
     $_SESSION['session_expire'] = time() + 1800;
 } else {
     $logout_status = 'timeout';
-    require_once(LOGOUT);
+    require_once LOGOUT;
 }
 
-if($page_admin) {
-    if($_SESSION['u_type'] != 'admin') {
-        header('location:/dashboard.php');
-    }
+if($page_admin && $_SESSION['u_type'] != 'admin') {
+    header('location:/dashboard.php');
 }
-?>
